2af86c707f9a49f66eccb4e5409f7a2788cd4d5e
Nelson Sugoi C <nelson.cvlhr@gmail.com>
2018-08-16 17:31:03 +0200

    added comments and explanations

--- lesson7/dataTypes.c
+++ lesson7/dataTypes.c
@@ -1,11 +1,11 @@
 /**
- * Data types : il existe deux groupes de données maeur :
+ * Data types : on distingue deux groupes de données :
  * Les données primitive (ou atomiques) et les données complexes.
  * 
- * Les données primitives sont les "plus petites" données possibles.
+ * Les données primitives sont les "plus petites" données possibles (ex : un byte).
  * "Petit" ne correspond pas à la taille ou au poids de la donnée, cela signifie juste que cette donnée est indivisible.
  * 
- * Les données complexes seraient ainsi des structures construites avec les données primitives.
+ * Les données complexes seraient ainsi des structures construites avec les données primitives (ex : une chaîne de caractères).
  * 
  * On peut ainsi faire le parallèle avec des légos (primitive = légo, complexe = construction en légo)
  */
